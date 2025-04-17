6e66bf669be95e840f5944d87625b77f4f060f0e
o.benkhalifa <o.benkhalifa@instadeep.com>
2025-04-17 16:59:05 +0100

    credentials

--- api/api/Infrastructure/DbContextFactory.cs
+++ api/api/Infrastructure/DbContextFactory.cs
@@ -1,3 +1,4 @@
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
@@ -7,9 +8,23 @@ public class DbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext
 {
     public ApplicationDbContext CreateDbContext(string[] args)
     {
+        // Load environment variables from .env file
+        Env.Load();
+
+        // Get environment variables for the database connection
+        var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+        var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+        var dbUsername = Environment.GetEnvironmentVariable("DB_USERNAME");
+        var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+        var dbName = Environment.GetEnvironmentVariable("DB_DATABASE");
+
+        // Build connection string
+        var connectionString = $"Host={dbHost};Port={dbPort};Username={dbUsername};Password={dbPassword};Database={dbName}";
+
+        // Create the DbContextOptions using the connection string
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        optionsBuilder.UseNpgsql(
-            "Host=localhost;Database=sportscoach;Username=postgres;Password=oumayma404!!!!");
-        return new(optionsBuilder.Options);
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
     }
 }
